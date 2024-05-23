84b9dacde49554a135b7ca1359333ac2d9f287fb
ahno <ahno@null>
2024-05-22 23:50:22 -0600

    very basic graph line drawing

--- src/graph.c
+++ src/graph.c
@@ -21,23 +21,20 @@ draw_graph_lines(struct graph *graph)
 		}
 	}
 
-/* 	int steps_x = (float)(graph->max_x - graph->min_x) / graph->step; */
-/* 	int stepsize_x = graph->pos.width / steps_x; */
+	int stepsize_y = graph->pos.width / (graph->max_y - graph->min_y);
+	for (int i = 0; i < graph->max_y - graph->min_y; i++) {
+		Vector2 start = {
+			graph->pos.x,
+			graph->pos.y + i * stepsize_y,
+		};
+		Vector2 end = {
+			graph->pos.x + graph->pos.width,
+			graph->pos.y + i * stepsize_y, 
+		};
 
-/* 	for (int i = 0; i < steps_x; i++) { */
-/* 		DrawLine(graph->pos.x + i * stepsize_x, graph->pos.y, */
-/* 				graph->pos.x + i * stepsize_x, */ 
-/* 				graph->pos.y + graph->pos.height, */
-/* 				BLACK); */
-/* 	} */
-
-/* 	int steps_y = (float)(graph->max_y - graph->min_y) / graph->step; */
-/* 	int stepsize_y = graph->pos.height / steps_y; */
-
-/* 	for (int i = 0; i < steps_y; i++) { */
-/* 		DrawLine(graph->pos.x, graph->pos.y + i * stepsize_y, */
-/* 				graph->pos.x + graph->pos.width, */
-/* 				graph->pos.y + i * stepsize_y, */
-/* 				BLACK); */
-/* 	} */
+		DrawLineEx(start, end, 1, GRAY);
+		if (graph->min_y + i == 0) {
+			DrawLineEx(start, end, 3, BLACK);
+		}
+	}
 }
